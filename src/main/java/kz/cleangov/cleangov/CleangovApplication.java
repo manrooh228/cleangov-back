6d93078e8ba724749dc8f3ebdfa2c3fad637792e
manrooh228 <maksatsuper57915@gmail.com>
2024-10-26 19:04:02 +0500

    Add files via upload

---
+++ src/main/java/kz/cleangov/cleangov/CleangovApplication.java
@@ -0,0 +1,13 @@
+package kz.cleangov.cleangov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleangovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleangovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/cleangov/cleangov/CleangovApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.cleangov.cleangov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleangovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
